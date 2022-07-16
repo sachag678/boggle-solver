7bfe9fec480d63c51356fd74c55c70a12a20c7e5
sachag678 <sacha.gunaratne@gmail.com>
2022-07-16 17:08:19 +0530

    feat: generalized to any size board
    added optimization to ignore partial word paths that will not lead to any actual words during the dfs which improves time complexity

--- find_words.py
+++ find_words.py
@@ -2,7 +2,16 @@ from nltk.corpus import wordnet
 from collections import defaultdict
 from os import system
 
-setofwords = set(wordnet.words())
+words = wordnet.words()
+
+setofwords = set(words)
+
+partialsetofwords = set()
+for word in setofwords:
+    for i in range(1, len(word)):
+        partialsetofwords.add(word[0:i])
+
+board_len = 5
 
 def check_word(word):
     return word in setofwords
@@ -11,8 +20,7 @@ def find_words(nodes):
     paths = []
     for n in nodes:
         dfs(n, paths=paths)
-        print('finding words...', end="\r")
-    words = [w for w in set(paths) if check_word(w)]
+    words = list(set(paths))
 
     words.sort(reverse=True, key=lambda x: len(x))
 
@@ -23,12 +31,12 @@ def find_words(nodes):
     print("--------")
 
     # print the box
-    for i in range(4):
-        for j in range(4):
+    for i in range(board_len):
+        for j in range(board_len):
             end = ""
-            if j == 3:
+            if j == board_len - 1:
                 end="\n"
-            print(nodes[i*4+j].value, " ", end=end)
+            print(nodes[i*board_len+j].value, " ", end=end)
         
     print("--------")
     
@@ -38,13 +46,17 @@ def find_words(nodes):
 
 def dfs(node, paths, visited=[], path=""):
     path = path + node.value
-    if len(path) >= 3:
-        paths.append(path)
+    if len(path) >= 3: # min word size
+        if check_word(path):
+            paths.append(path)
     for neighbour in node.neighbours:
-        if neighbour.loc not in visited:
+        if neighbour.loc not in visited and fuzzy_match(path):
             new_visited = [neighbour.loc, node.loc]
             new_visited.extend(visited)
             dfs(neighbour, paths, new_visited, path)
+
+def fuzzy_match(partial_word):
+    return partial_word in partialsetofwords
 
 class Node:
 
@@ -65,11 +77,15 @@ class Node:
 
 nodes = []
 
-locs = [(i, j) for i in range(0, 4) for j in range(0, 4)]
+locs = [(i, j) for i in range(0, board_len) for j in range(0, board_len)]
+
+flag = str(input('Use default board(b) or enter new one(n)? '))
+
+board = ['e', 'f', 't', 'w', 'n', 't', 'i', 'e', 'r', 'b', 'w', 'qu', 's', 'g', 'y', 'f', 'e', 't', 'r', 'n', 'c', 'h', 'd', 'i', 'n']
 
 print("Enter letters row wise starting from the top, going left to right.")
-for i in range(16):
-    letter = str(input("enter letter: "))
+for i in range(board_len ** 2):
+    letter = board[i] if flag == 'b' else str(input("enter letter: "))
     node = Node(letter, locs[i])
     nodes.append(node)
 
@@ -79,5 +95,5 @@ for n in nodes:
     for nei in nodes:
         if (n.loc[0] + 1 == nei.loc[0] and n.loc[1] == nei.loc[1]) or (n.loc[0] - 1 == nei.loc[0] and n.loc[1] == nei.loc[1]) or (n.loc[1] + 1 == nei.loc[1] and n.loc[0] == nei.loc[0]) or (n.loc[1] - 1 == nei.loc[1] and n.loc[0] == nei.loc[0]) or (n.loc[0] + 1 == nei.loc[0] and n.loc[1] + 1 == nei.loc[1]) or (n.loc[0] - 1 == nei.loc[0] and n.loc[1] - 1 == nei.loc[1]) or (n.loc[0] + 1 == nei.loc[0] and n.loc[1] - 1 == nei.loc[1]) or (n.loc[0] - 1 == nei.loc[0] and n.loc[1] + 1 == nei.loc[1]):
             n.add_neighbour(nei)
-
+print('finding words...')
 find_words(nodes)
